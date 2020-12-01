697e1f37f22306c46a33d40761a9a9dbd3995d12
Andy Cheng <acheng@codingmachine.com>
2020-11-30 23:53:31 -0800

    add new routing for admin page and channel page

--- stripe_payment/stripePaymentService/routes/index.js
+++ stripe_payment/stripePaymentService/routes/index.js
@@ -9,17 +9,19 @@ var router = express.Router();
 let acct = require('../acct/controllers/acctController');
 let prod = require('../prod/controllers/prodController');
 let cust = require('../cust/controllers/custController');
+let adm = require('../adm/controllers/admController');
 
 router.post('/acct/:phone', acct.new_acct);
-router.post('/cust/:phone', acct.new_cust);
+router.post('/cust/:phone', cust.new_cust);
 
 router.delete('/acct/:id', acct.delete_acct);
+router.delete('/cust/:id', cust.delete_cust);
 router.get('/acct_link/:id', acct.acct_link);
 
 router.post('/prod', prod.new_prod);
 
 router.get('/accts', acct.all_accts);
 router.get('/prods', prod.all_prods);
-router.get('/prices', prod.all_prices);
+router.get('/custs', cust.all_custs);
 
 module.exports = router;
