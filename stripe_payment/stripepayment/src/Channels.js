0e0569c33d9684d70c99caa4d530d15e1435ed76
Andy Cheng <acheng@codingmachine.com>
2020-12-02 23:30:18 -0800

    Add listing of channels and subscription button

--- stripe_payment/stripepayment/src/Channels.js
+++ stripe_payment/stripepayment/src/Channels.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from './img/qplogo.png'
 import './App.css'
+import Table from 'react-bootstrap/Table'
 
 export default function Channels() {
   // Show available channels and form to add new channels
   const [productName, setproductName] = useState('');
   const [productPrice, setproductPrice] = useState('');
   const [user, setUser] = useState('');
+  const [prodList, setProdList] = useState([]);
 
-  function reload() {
+  const SubIcon = () => {
+    return (
+      <span title="Subscribe">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-cart-check-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM4 14a1 1 0 1 1 2 0 1 1 0 0 1-2 0zm7 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0zm.354-7.646a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+        </svg>
+      </span>
+    )
+  }
+
+  useEffect(() => {
+    refreshChannels();
+  }, []);
+
+  function clearFields() {
     setproductName('');
     setproductPrice('');
     setUser('');
@@ -21,98 +37,117 @@ export default function Channels() {
     if (n === 'user') setUser(event.target.value);
   }
 
+  function addChannel(ownerAccount, ownerPhone) {
+    console.log('creating product with name:' + productName + 'price:' + productPrice + 'acct:' + ownerAccount + 'phone:' + ownerPhone);
+
+    // Set up product and pricing
+    fetch("http://localhost:3000/prod", {
+            method: 'post',
+            body: 'name=' + productName + '&price=' + productPrice + '&user=' + ownerAccount + '&phone=' + ownerPhone,
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            }
+          })
+          .then(res => {
+            console.log('Added a new channel');
+            refreshChannels();
+          })
+          .catch(
+            (error) => {
+                alert("ERROR adding a new channel:" + error);
+            }
+          )
+  }
+
+  function refreshChannels() {
+
+    // get list of products (prices objects)
+    fetch("http://localhost:3000/prods")
+      .then(res => {
+        res.json()
+           .then(data => {
+              var returnList = data.data;
+              setProdList(returnList);
+              clearFields();
+           })
+          .catch(err => console.log('json() error:' + err))
+      })
+      .catch(err => console.log('res error:' +err))
+  }
+
   /**
    * For first time channel creator, also create connect account
    */
   function handleAdd(event) {
     event.preventDefault();
 
-    const acct = '';
-    const phone = '';
-
     if (!user.startsWith("acct_")) {
 
       // Set up new connect account
-      fetch("http://localhost:3000/acct", {
+      fetch("http://localhost:3000/acct/" + user, {
         method: 'post',
-        body: 'name=' + productName + '&price=' + productPrice + '&user=' + acct + '&phone=' + phone,
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        }
+        // body: 'phone=' + user,
+        // headers: {
+        //   'Content-Type': 'application/x-www-form-urlencoded',
+        // }
       })
       .then(res => {
-        console.log('Added a new channel' + res);
-        reload();
+        res.json()
+           .then(data => {
+              alert('Please complete on boarding here: ' + data.link);
+              addChannel(data.id, user)
+           })
+           .catch(err => console.log('json() error:' + err))
       })
       .catch(
         (error) => {
-            alert("ERROR adding a new channel:" + error);
-            reload();
+            alert("ERROR creating new connect account" + error);
         }
       )
     }
+    else {
+      // TODO populate phone field by retrieving account
+      addChannel(user, user)
+    }
+  }
 
-    console.log('new channel');
-
-    fetch("http://localhost:3000/prod", {
-            method: 'post',
-            body: 'name=' + productName + '&price=' + productPrice + '&user=' + acct + '&phone=' + phone,
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-            }
-          })
-          .then(res => {
-            console.log('Added a new channel' + res);
-            reload();
-          })
-          .catch(
-            (error) => {
-                alert("ERROR adding a new channel:" + error);
-                reload();
-            }
-          )
+  function handleSubscribe(ch) {
+    console.log('Ready to subscribe to:' + ch);
+    const v = document.getElementById(ch).value;
+    console.log('customer entered:' + v);
   }
 
   const ListChannels = () => {
-    fetch("http://localhost:3000/prods")
-    .then(res => {
-      console.log('response status:' + res.status);
-      res.json()
-         .then(data => console.log('got it:' + JSON.stringify(data)))
-         .catch(err => console.log('json() error:' + err))
-    })
-    .catch(err => console.log('res error:' +err))
-
     
-    // .then(res => {
-    //   console.log('listing channels response:' + res.json());
-      // res.data.map((pd) => {
-      //   const nm = pd.metadata.channel;
-      //   console.log('nm' + nm);
-      //   const pc = pd.unit_amount;
-      //   console.log('pc' + pc);
-      //   const ur = pd.metadata.user;
-      //   console.log('ur' + ur);
-      //   return (
-      //       <div>
-      //           <div style={{float:'left'}}>Channel Name: {nm}</div>
-      //           <div style={{float:'left'}}>Price: {pc} cents</div>
-      //           <div style={{float:'left'}}>Owner: {ur}</div>
-      //       </div>
-      //   )
-      // })
-    // })
-    // .catch(
-    //   (error) => {
-    //       alert("ERROR listing channels:" + error);
-    //       return (
-    //         <h3>ERROR!</h3>
-    //       )
-    //   }
-    // )
-    return(
-      <div>happy</div>
-    )
+    if (prodList && prodList.length > 0) {
+
+      return prodList.map((pd) => {
+        const nm = pd.metadata.channel;
+        return (
+          <tr key={nm}>
+            <td>{nm} ({pd.unit_amount}¢)</td>
+            <td>{pd.metadata.user}</td>
+            <td>{pd.metadata.phone}</td>
+            <td>
+              <label>
+                  <input id={nm} type="text" name="sub" size="10" maxLength="20" placeholder="customer"/>
+              </label>
+              <button onClick={() => handleSubscribe(nm)}><SubIcon/></button>
+            </td>
+          </tr>
+        )
+      })
+    }
+    else {
+      return (
+        <tr>
+          <td>No Channels</td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      );
+    }
   }
 
   return (
@@ -134,7 +169,19 @@ export default function Channels() {
           </form>
         </div>
         <div>
-          <ListChannels/>
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>Channel</th>
+                <th>Owner</th>
+                <th>Owner Phone</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <ListChannels/>
+            </tbody>
+          </Table>
         </div>
       </div>
     </div>
